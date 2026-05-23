160a1ebc05336c94181b736b5a28a3c5b0bbd7f1
Gohel Yash <yashgohel0000@gmail.com>
2026-05-23 21:25:35 +0530

    Enforce secure HTTPS for Cloudinary URLs

--- socialvibe/socialvibe/settings.py
+++ socialvibe/socialvibe/settings.py
@@ -137,6 +137,7 @@ if os.environ.get('CLOUDINARY_CLOUD_NAME'):
         'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
         'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
         'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+        'SECURE': True,
     }
     STORAGES = {
         "default": {
