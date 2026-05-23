8f781df346399fea8163edf081999324e9520e53
Gohel Yash <yashgohel0000@gmail.com>
2026-05-23 20:58:58 +0530

    initialize Django WSGI configuration and update system path for deployment

--- socialvibe/socialvibe/wsgi.py
+++ socialvibe/socialvibe/wsgi.py
@@ -15,6 +15,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.append(str(BASE_DIR))
 
+from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socialvibe.settings')
 
 application = get_wsgi_application()
